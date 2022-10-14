0f0c84f96c00547bfa99cced324a60809a6c3e17
NUCCASJNR <adegbitealareef28@yahoo.com>
2022-10-14 03:34:21 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Rewturn:Always 0
+ */
+int main(void)
+{
+	int i;
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
