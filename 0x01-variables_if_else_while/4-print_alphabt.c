4e3470eda2fa984595c3b6c064d41ccf7f3f18e9
NUCCASJNR <adegbitealareef28@yahoo.com>
2022-10-14 03:27:07 -0700

    q and e are exempted

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * return: Always 0
+ */
+int main(void)
+{
+	char low;
+	for (low = 'a'; low <= 'z'; low++)
+		if (low == 'q' || low == 'e')
+			continue;
+				putchar(low);
+	putchar('\n');
+	return (0);
+}
