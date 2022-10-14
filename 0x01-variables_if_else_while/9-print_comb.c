709a2235415634ce8a93e4f2b952659176528be4
NUCCASJNR <adegbitealareef28@yahoo.com>
2022-10-14 07:29:13 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,13 +5,16 @@
  */
 int main(void)
 {
-	int d;
-
-	for (d = '0'; d <= '9'; d++)
-	putchar(d);
-	if (d != '9')
-	putchar(',');
-	putchar(' ');
+	int x;
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+		if (x < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
 	return (0);
 }
