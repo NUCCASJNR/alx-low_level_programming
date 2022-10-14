00cba585e0f0803dad52486bc3b291150f7a7d1a
NUCCASJNR <adegbitealareef28@yahoo.com>
2022-10-14 04:37:21 -0700

    fix print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-	char low;
-	for (low = 'a' ; low <= 'z'; low++);
-	putchar(low);
-	putchar ('\n');
-	return (0);
+char low;
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+putchar ('\n');
+return (0);
 }
