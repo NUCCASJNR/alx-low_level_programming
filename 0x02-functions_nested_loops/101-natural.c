4f7821d3b0ece66a30d67737b36cd6f641c7faf7
NUCCASJNR <adegbitealareef28@yahoo.com>
2022-10-18 06:31:12 -0700

    natural;

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Print all multiples of 3 & 5 below 1024
+ * Return: 0
+ */
+int main(void)
+{
+	int n, sum;
+
+	while (n < 1024)
+	{
+		if ((n % 3) == 0 || (n % 5) == 0)
+		{
+			sum += n;
+		}
+		n++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
