6fe604011634cd5ebf5fed6259db7f7a1334d449
NUCCASJNR <adegbitealareef28@yahoo.com>
2022-10-18 04:55:22 -0700

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@ void jack_bauer(void)
 {
 	int H, M;
 
-	for (H = 0; H <= 24; H++)
+	for (H = 0; H < 24; H++)
 	{
-		for (M = 0; M <= 60; M++)
+		for (M = 0; M < 60; M++)
 		{
 			_putchar((H / 10) + '0');
 			_putchar((H % 10) + '0');
