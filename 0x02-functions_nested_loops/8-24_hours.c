4589777e423180e99037609a15a03ba2a3ad53fc
NUCCASJNR <adegbitealareef28@yahoo.com>
2022-10-18 04:49:37 -0700

    hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - a function that prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59
+ * Return: Nothing
+ */
+void jack_bauer(void)
+{
+	int H, M;
+
+	for (H = 0; H <= 24; H++)
+	{
+		for (M = 0; M <= 60; M++)
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
