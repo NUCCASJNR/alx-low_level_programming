242ac464bf197540c67868b9e1a04160e5842193
NUCCASJNR <adegbitealareef28@yahoo.com>
2022-10-19 12:28:10 -0700

    readme

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
  * main - Entry point
  *
@@ -9,13 +7,14 @@
 int main(void)
 {
 	char low;
-	{
-
+	
 	for (low = 'a'; low <= 'z'; low++)
+	{
 		if (low != 'e' && low != 'q')
 			continue;
 				putchar(low);
-	putchar('\n');
 	}
+
+	putchar('\n');
 	return (0);
 }
--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -20,7 +20,7 @@ int main(void)
 
 
 
-		        i = 98;
+		        i = 0;
 
 			        positive_or_negative(i);
 
