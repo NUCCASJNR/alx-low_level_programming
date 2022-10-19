1adc9a669fed5afb4fde21992ac9593ef4eb932d
NUCCASJNR <adegbitealareef28@yahoo.com>
2022-10-19 11:31:35 -0700

    all

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
+
 #define MAIN_H
+
 
 #include <stdio.h>
 
@@ -7,5 +9,4 @@ void positive_or_negative(int i);
 int largest_number(int, int, int);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
-
 #endif /* MAIN_H */
