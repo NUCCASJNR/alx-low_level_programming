1380d4fb4c6cef8e3f28becf7898139c6d4fd012
NUCCASJNR <adegbitealareef28@yahoo.com>
2022-10-20 06:18:23 -0700

    2 and 4 are exempted!

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  print_most_numbers -  a function that prints the numbers, from 0 to 9,
+ * followed by a new line.
+ *  except 2 and 4
+ *  Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a != '2' && a != '4')
+	_putchar(a);
+	}
+	_putchar('\n');
+}
