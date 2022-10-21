1de3b54046a1224a8009eb8231125769cbf1a61b
NUCCASJNR <adegbitealareef28@yahoo.com>
2022-10-21 08:30:47 -0700

    diagonal!!!!!!!

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,15 +6,19 @@
  */
 void print_diagonal(int n)
 {
-	int i, int j;
+	int i, j;
 
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
-			_putchar('\');
-		}
+			for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar(92);
+		_putchar('\n');
 	}
-	_putchar('\n');
+	}
+	else
+		_putchar('\n');
 }
 
