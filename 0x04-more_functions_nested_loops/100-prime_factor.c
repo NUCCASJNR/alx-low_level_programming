12fd5a81a6a5233515644df1228707ccd9495929
NUCCASJNR <adegbitealareef28@yahoo.com>
2022-10-22 13:08:42 -0700

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - a program that finds and prints the largest prime factor
+ *of the number 612852475143'
+ * followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long int i = 3, n = 612852475143;
+
+	for (; i < 12057; i += 2)
+	{
+		while (n % i == 0 && n != i)
+			n /= i;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
