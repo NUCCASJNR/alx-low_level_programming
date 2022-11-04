621e3e845f3384a9f5b4602e34b0dc93e2958725
NUCCASJNR <adegbitealareef28@yahoo.com>
2022-11-04 08:31:21 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: input integer
+ * @argv: input character
+ * Return: Always 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
