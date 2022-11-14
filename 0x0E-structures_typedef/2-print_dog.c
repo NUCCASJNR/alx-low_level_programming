704cf0aa84850791ee6aedc98e4fcaea4f2bc773
NUCCASJNR <adegbitealareef28@yahoo.com>
2022-11-14 14:36:48 -0800

    al

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - main fuction
+ * @d: pointer
+ * Return: Always 0
+ */
+
+void print_dog(struct dog *d)
+{
+
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	printf("Age: %.6f\n", d->age);
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+}
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+
+/**
+ * _string - function holding the ptr
+ * @point: pointer holding the values
+ * Return: Always 0
+ */
+char *_string(char *point)
+{
+	char *ptr;
+	int i;
+	int len = 0;
+
+	if (point == NULL)
+	{
+		return (NULL);
+	}
+	while (point[len] != '\0')
+	{
+		len++;
+	}
+	ptr = malloc(sizeof(char) * (len + 1));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; point[i] != '\0'; i++)
+	{
+		ptr[i] = point[i];
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+/**
+ * new_dog - Funcion to create a new dog
+  * new_dog - Funcion to create a new dog
+ * @name: initial name of dog
+ * @age: initial dog age
+ * @owner: initial dog owner
+ * Return: Always 0
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	char *dog_name, *dog_owner;
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		return (NULL);
+	}
+	dog_name = _string(name);
+	if (dog_name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->name = dog_name;
+
+	dog->age = age;
+
+	dog_owner = _string(owner);
+	if (dog_owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = dog_owner;
+	return (dog);
+}
