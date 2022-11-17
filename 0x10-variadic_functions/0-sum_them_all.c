c4c39fe09e0d3812e2f87d4855b97ab2af47e1ac
NUCCASJNR <adegbitealareef28@yahoo.com>
2022-11-17 12:48:28 -0800

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,20 +9,24 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i;
+	unsigned int a;
 	int sum = 0;
-
 	va_list args;
 
-	if (n == 0)
-	return (0);
-
 	va_start(args, n);
-	for (i = 0; i < n; i++)
+
+	for (a = 0; a < n; a++)
 	{
-		sum += va_arg(args, int);
+		if (n == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			sum += va_arg(args, int);
+		}
 	}
-va_end(args);
-return (sum);
+	va_end(args);
+	return (sum);
 }
 
