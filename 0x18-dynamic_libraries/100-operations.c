9369235316d6b207b169de5339fb415ccc78394a
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-01-17 07:18:29 -0800

    dynamic libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,28 @@
+int add(a, b)
+{
+	return (a + b);
+}
+int sub(a, b)
+{
+	return (a - b);
+}
+int mul(a, b)
+{
+	return (a * b);
+}
+int div(a, b)
+{
+	if (b != 0)
+	{
+		return (a / b);
+	}
+	return (0);
+}
+int mod(a, b)
+{
+	if (b != 0)
+	{
+		return (a % b);
+	}
+	return (0);
+}
