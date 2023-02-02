44448b83420cc32cc5ef85e60a078dd7290f37ca
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-02-02 14:33:03 -0800

    update

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 
 /**
- * int key_index: a function that gives you the index of a key.
- * @key - Key
- * @size - size of the array of the hash table
+ * key_index - a function that gives you the index of a key.
+ * @key : Key
+ * @size : size of the array of the hash table
  * Return: the index at which the key/value pair should be stored in the
  *  array of the hash table
  */
