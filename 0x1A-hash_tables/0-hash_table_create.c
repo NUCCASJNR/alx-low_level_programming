5ddb847b6ba7215e8cb8c3e7779317f7e38e9550
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-02-02 14:07:36 -0800

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,14 +10,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash;
 	unsigned long int i;
-	
+
 	hash = malloc(sizeof(hash_table_t));
 
 	if (hash == NULL)
 		return (NULL);
 
 	hash->size = size;
-	hash->array = malloc(sizeof(hash_table_t)* size);
+	hash->array = malloc(sizeof(hash_table_t) * size);
 	if (hash->array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
